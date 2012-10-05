424f69b98a7c67098fe11145a780a758e0668a25
Thobias Bergqvist <thobias.bergqvist@gmail.com>
2012-10-05 15:29:09 +0200

    added config project

---
+++ config/config-server/src/main/java/se/lingonskogen/config/server/App.java
@@ -0,0 +1,13 @@
+package se.lingonskogen.config.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
