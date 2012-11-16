295fd1c25ad4e24c73d8671190598bca9a6e6306
Thobias Bergqvist <thobias.bergqvist@gmail.com>
2012-11-16 01:16:02 +0100

    added new rest resource

--- restdemo/src/main/java/se/sandbox/restdemo/RestResource.java
+++ restdemo/src/main/java/se/sandbox/restdemo/RestResource.java
@@ -2,91 +2,127 @@ package se.sandbox.restdemo;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import se.sandbox.restdemo.generic.Field;
 import se.sandbox.restdemo.generic.Form;
 import se.sandbox.restdemo.generic.Item;
 import se.sandbox.restdemo.generic.Items;
-import se.sandbox.restdemo.generic.Link;
-import se.sandbox.restdemo.generic.Msg;
-import se.sandbox.restdemo.generic.Prop;
 import se.sandbox.restdemo.providers.JsonStringMarshaller;
 import se.sandbox.restdemo.providers.StringMarshaller;
 
 @Path("")
+@Produces(MediaType.APPLICATION_JSON)
 public class RestResource
 {
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("form")
-    public Response getForm() {
-        Form form = new Form();
-        form.setLabel("My Form");
-        form.setHref("/form");
-        form.setRel(MediaType.APPLICATION_JSON);
-        form.setVerb("POST");
-        for (int i = 0; i < 4; i++)
-        {
-            Field field = new Field();
-            field.setLabel("Label-" + i);
-            field.setId("id-" + i);
-            field.setType("string");
-            field.setValue("<default>");
-            form.getFields().add(field);
-            Msg msg = new Msg();
-            msg.setLevel("WARNING");
-            msg.setLabel("Warning!");
-            msg.setValue("Warning number " + i);
-            field.getMsgs().add(msg);
-        }
-        for (int i = 0; i < 2; i++)
-        {
-            Msg msg = new Msg();
-            msg.setLevel("ERROR!");
-            msg.setLabel("Error");
-            msg.setValue("Error number " + i);
-            form.getMsgs().add(msg);
-        }
-        StringMarshaller marshaller = new JsonStringMarshaller();
-        String str = marshaller.marshall(form);
-        return Response.ok(str).build();
-    }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("items")
-    public Response getItems() {
+    public Response getRoot() {
+        Builder builder = new Builder();
+        StringMarshaller marshaller = new JsonStringMarshaller();
+
         Items items = new Items();
-        for (int i = 0; i < 2; i++)
+        items.getLinks().add(builder.buildLink("List Foos", "/foos"));
+        items.getLinks().add(builder.buildLink("List Bars", "/bars"));
+        
+        String content = marshaller.marshall(items);
+        return Response.ok(content).build();
+    }
+    
+    @GET
+    @Path("foos")
+    public Response getListFoos() {
+        
+        Builder builder = new Builder();
+        StringMarshaller marshaller = new JsonStringMarshaller();
+
+        Items items = new Items();
+        for (int i = 0; i < 3; i++)
         {
             Item item = new Item();
-            for (int j = 10; j < 12; j++)
+            for (int j = 0; j < 1; j++)
             {
-                Prop prop = new Prop();
-                prop.setLabel("Prop-" + i);
-                prop.setId("id-" + j);
-                prop.setType("string");
-                prop.setValue("value-" + j);
-                item.getProps().add(prop);
-                Link link = new Link();
-                link.setLabel("Link-" + j);
-                link.setHref("/link-" + j);
-                link.setRel(MediaType.APPLICATION_JSON);
-                item.getLinks().add(link);
+                item.getProps().add(builder.buildProp("Prop-" + j));
             }
+            item.getLinks().add(builder.buildLink("View", "/foos/" + i));
             items.getItems().add(item);
-            Link link = new Link();
-            link.setLabel("Link-" + i);
-            link.setHref("/link-" + i);
-            link.setRel(MediaType.APPLICATION_JSON);
-            items.getLinks().add(link);
         }
-        StringMarshaller marshaller = new JsonStringMarshaller();
-        String str = marshaller.marshall(items);
-        return Response.ok(str).build();
+        items.getLinks().add(builder.buildLink("Create Foo", "/create/foo"));
+        
+        String content = marshaller.marshall(items);
+        return Response.ok(content).build();
     }
 
+    @GET
+    @Path("foos/{index}")
+    public Response getViewFoo(@PathParam("index") String index) {
+        
+        Builder builder = new Builder();
+        StringMarshaller marshaller = new JsonStringMarshaller();
+
+        Item item = new Item();
+        for (int j = 0; j < 3; j++)
+        {
+            item.getProps().add(builder.buildProp("Prop-" + j));
+        }
+        item.getLinks().add(builder.buildLink("View", "/foos/" + index));
+        item.getLinks().add(builder.buildLink("Update", "/update/foo/" + index));
+        item.getLinks().add(builder.buildLink("Delete", "/delete/foo/" + index));
+        
+        String content = marshaller.marshall(item);
+        return Response.ok(content).build();
+    }
+
+    @GET
+    @Path("create/foo")
+    public Response getCreateFoo() {
+        
+        Builder builder = new Builder();
+        StringMarshaller marshaller = new JsonStringMarshaller();
+
+        Form form = builder.buildForm("Create Foo", "/create/foo", "POST");
+        for (int i = 0; i < 3; i++)
+        {
+            form.getFields().add(builder.buildField("Prop-" + i));
+        }
+        
+        String content = marshaller.marshall(form);
+        return Response.ok(content).build();
+    }
+
+    @GET
+    @Path("update/foo/{index}")
+    public Response getUpdateFoo(@PathParam("index") String index) {
+        
+        Builder builder = new Builder();
+        StringMarshaller marshaller = new JsonStringMarshaller();
+
+        Form form = builder.buildForm("Update Foo", "/update/foo/" + index, "PUT");
+        for (int i = 0; i < 3; i++)
+        {
+            form.getFields().add(builder.buildField("Prop-" + i));
+        }
+        
+        String content = marshaller.marshall(form);
+        return Response.ok(content).build();
+    }
+
+    @GET
+    @Path("delete/foo/{index}")
+    public Response getDeleteFoo(@PathParam("index") String index) {
+        
+        Builder builder = new Builder();
+        StringMarshaller marshaller = new JsonStringMarshaller();
+
+        Form form = builder.buildForm("Update Foo", "/update/foo/" + index, "DELETE");
+        for (int i = 0; i < 3; i++)
+        {
+            form.getFields().add(builder.buildField("Prop-" + i));
+        }
+        form.getMsgs().add(builder.buildMsg("WARNING", "Delete!", "Are your sure?"));
+        String content = marshaller.marshall(form);
+        return Response.ok(content).build();
+    }
 }
