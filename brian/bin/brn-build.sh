eb31456ac8b4c5fd819597bef9f9e7af0ecc350c
Thobias Bergqvist <thobias.bergqvist@gmail.com>
2012-11-27 13:48:31 +0100

    refactor...

--- brian/bin/brn-build.sh
+++ brian/bin/brn-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+APP_NAME=`basename $0`
 DIR_BIN=`dirname $0`
 DIR_WWW=/home/tobbe/www/brian/data
 
@@ -7,62 +8,112 @@ DIR_LC=${DIR_WWW}/large-cap
 DIR_MC=${DIR_WWW}/mid-cap
 DIR_SC=${DIR_WWW}/small-cap
 
-DIR_LC_QUOTES=${DIR_WWW}/large-cap/quotes
-DIR_MC_QUOTES=${DIR_WWW}/mid-cap/quotes
-DIR_SC_QUOTES=${DIR_WWW}/small-cap/quotes
+DIR_LC_SAMPLE=${DIR_LC}/samples
+DIR_MC_SAMPLE=${DIR_MC}/samples
+DIR_SC_SAMPLE=${DIR_SC}/samples
 
-function build() {
-    local DIR=$1
-    local DIR_QUOTES=$2
-    local DATE=$3
+DIR_LC_QUOTES=${DIR_LC}/quotes
+DIR_MC_QUOTES=${DIR_MC}/quotes
+DIR_SC_QUOTES=${DIR_SC}/quotes
 
-    ${DIR_BIN}/brn-names.sh
+NOW_DATE=`date +%Y-%m-%d`
 
-    if [ -s ${DIR}/names.csv ]; then
+DIR_CSV_LC=${DIR_LC_QUOTES}/${NOW_DATE}
+DIR_CSV_MC=${DIR_MC_QUOTES}/${NOW_DATE}
+DIR_CSV_SC=${DIR_SC_QUOTES}/${NOW_DATE}
 
-	mkdir -p ${DIR_QUOTES}
-
-        # dates
-	echo "Dates:  (csv) ${DIR}/dates.csv"
-	ls ${DIR_QUOTES} | egrep "[0-9]+-[0-9]+-[0-9]+" > ${DIR}/dates.csv
-
-	echo "Dates: (json) ${DIR}/dates.json"
-	local PAD=""
-	echo -n "{\"dates\":[" > ${DIR}/dates.json
-	while read LINE; do
-	    echo "$PAD" >> ${DIR}/dates.json
-	    echo -n "  \"$LINE\"" >> ${DIR}/dates.json
-	    PAD=","
-	done < ${DIR}/dates.csv
-	echo "]}" >> ${DIR}/dates.json
-
-        # quotes
-	echo "Build: (json) ${DIR_QUOTES}/${DATE}"
-	for KEY in `cat ${DIR}/names.csv | cut -d ',' -f 1`; do
-	    #echo "Build: (json) ${DIR_QUOTES}/${DATE}/${KEY}.json"
-    	    local PAD=""
-    	    echo -n "{\"quotes\":[" > ${DIR_QUOTES}/${DATE}/${KEY}.json
-	    for LINE in `cat ${DIR_QUOTES}/${DATE}/${KEY}.csv`; do
-    		echo "$PAD" >> ${DIR_QUOTES}/${DATE}/${KEY}.json
-    		echo -n "{$LINE}" >> ${DIR_QUOTES}/${DATE}/${KEY}.json
-    		PAD=","
-    	    done
-    	    echo "]}" >> ${DIR_QUOTES}/${DATE}/${KEY}.json
-	done
-    fi
+function msg() {
+    local STAMP=`date "+%Y-%m-%d %H:%M:%S"`
+    echo "[${APP_NAME} @ ${STAMP}] $1"
 }
 
-if [ $# == 0 ]; then
-    DATE=`date +%Y-%m-%d`
-    build ${DIR_LC} ${DIR_LC_QUOTES} ${DATE}
-    build ${DIR_MC} ${DIR_MC_QUOTES} ${DATE}
-    build ${DIR_SC} ${DIR_SC_QUOTES} ${DATE}
-else
-    while [ "$1" ]; do
-	DATE=$1
-	build ${DIR_LC} ${DIR_LC_QUOTES} ${DATE}
-	build ${DIR_MC} ${DIR_MC_QUOTES} ${DATE}
-	build ${DIR_SC} ${DIR_SC_QUOTES} ${DATE}
-	shift
+function dates() {
+    local DIR_SAMPLE=$1
+    local DIR_TARGET=$2
+    if [ ! -d ${DIR_SAMPLE} ]; then
+	msg "Dates: Not Found! ${DIR_SAMPLE}"
+	return
+    else
+	msg "Dates: ${DIR_TARGET}/dates.json"
+    fi
+    ls ${DIR_SAMPLE} | egrep "[0-9]+-[0-9]+-[0-9]+" | sort > ${DIR_TARGET}/dates.csv
+
+    echo -n "{\"dates\":[" > ${DIR_TARGET}/dates.json
+    PAD=""
+    while read LINE; do
+	echo -n "${PAD}\"${LINE}\"" >> ${DIR_TARGET}/dates.json
+	PAD=","
+    done < ${DIR_TARGET}/dates.csv
+    echo -n "]}" >> ${DIR_TARGET}/dates.json
+}
+
+function names() {
+    local DIR_SAMPLE=$1
+    local DIR_TARGET=$2
+    if [ ! -d ${DIR_SAMPLE} ]; then
+	msg "Names: Not Found! ${DIR_SAMPLE}"
+	return
+    else
+	msg "Names: ${DIR_TARGET}/names.json"
+    fi
+    echo -n "" >> /tmp/brn-names.csv
+    for FILE_HTML in `find ${DIR_SAMPLE} -name "*.html"`; do
+	/usr/bin/xsltproc --html ${DIR_BIN}/brn-names-csv.xsl ${FILE_HTML} >> /tmp/brn-names.csv
     done
-fi
+    cat /tmp/brn-names.csv | sort | uniq > ${DIR_TARGET}/names.csv
+    rm /tmp/brn-names.csv
+
+    echo -n "{\"names\":[" > ${DIR_TARGET}/names.json
+    PAD=""
+    while read LINE; do
+	KEY=${LINE%%,*}
+	LINE=${LINE#*,}
+	echo -n "${PAD}{\"key\":\"$KEY\",\"name\":\"$LINE\"}" >> ${DIR_TARGET}/names.json
+	PAD=","
+    done < ${DIR_TARGET}/names.csv
+    echo -n "]}" >> ${DIR_TARGET}/names.json
+}
+
+function build() {
+    local DIR_CSV=$1
+    if [ ! -d ${DIR_CSV} ]; then
+	msg "Dates: Not Found! ${DIR_CSV}"
+	return
+    else
+	msg "Build: ${DIR_CSV}"
+    fi
+    for FILE_CSV in `ls ${DIR_CSV} | grep csv`; do
+	local FILE_JSON=`echo ${FILE_CSV} | sed 's/csv/json/g'`
+	echo -n "{\"date\":\"${NOW_DATE}\"," > ${DIR_CSV}/${FILE_JSON}
+	echo -n "\"quotes\":[" >> ${DIR_CSV}/${FILE_JSON}
+	PAD=""
+	while read LINE; do
+	    ATIME=${LINE%%,*}
+	    LINE=${LINE#*,}
+	    BTIME=${LINE%%,*}
+	    LINE=${LINE#*,}
+	    VOLUME=${LINE%%,*}
+	    LINE=${LINE#*,}
+	    LATEST=${LINE%%,*}
+	    LINE=${LINE#*,}
+	    BUY=${LINE%%,*}
+	    LINE=${LINE#*,}
+	    SELL=${LINE%%,*}
+	    echo -n "${PAD}{\"atime\":\"${ATIME}\",\"btime\":\"${BTIME}\",\"volume\":\"${VOLUME}\",\"latest\":\"${LATEST}\",\"buy\":\"${BUY}\",\"sell\":\"${SELL}\"}" >> ${DIR_CSV}/${FILE_JSON}
+	    PAD=","
+	done < ${DIR_CSV}/${FILE_CSV}
+	echo -n "]}" >> ${DIR_CSV}/${FILE_JSON}
+    done
+}
+
+build ${DIR_CSV_LC}
+build ${DIR_CSV_MC}
+build ${DIR_CSV_SC}
+
+names ${DIR_LC_SAMPLE} ${DIR_LC}
+names ${DIR_MC_SAMPLE} ${DIR_MC}
+names ${DIR_SC_SAMPLE} ${DIR_SC}
+
+dates ${DIR_LC_SAMPLE} ${DIR_LC}
+dates ${DIR_MC_SAMPLE} ${DIR_MC}
+dates ${DIR_SC_SAMPLE} ${DIR_SC}
--- brian/web/js/brian.js
+++ brian/web/js/brian.js
@@ -10,15 +10,8 @@ var stored = {
     nameStr: "",
     dateKey: ""
 }
-/*
-    var nameKey = "";
-    var nameStr = "";
-    var dateKey = makeDateKey(new Date());
-    var quotes = [];
-    var predicts = [];
-*/
 
-/* setters */
+/* setup */
 
 function init(capKey, labelId, canvasId, datesId, namesId) {
     meta.capKey = capKey;
@@ -28,11 +21,11 @@ function init(capKey, labelId, canvasId, datesId, namesId) {
     meta.namesId = namesId;
     s = sessionStorage[capKey + "-stored"];
     if (s == null) {
-	nowKey = makeDateKey(new Date());
+	//nowKey = makeDateKey(new Date());
 	stored = {
 	    nameKey: "",
 	    nameStr: "",
-	    dateKey: nowKey
+	    dateKey: ""
 	}
     } else {
 	stored = JSON.parse(s);
@@ -40,7 +33,6 @@ function init(capKey, labelId, canvasId, datesId, namesId) {
     //$("#log").html("stored:" + JSON.stringify(stored));
     loadDates();
     loadNames();
-    reload();
 }
 
 function updateStore() {
@@ -53,30 +45,21 @@ function updateStore() {
 function loadDates() {
     console.log("Loading " + meta.capKey + " dates ... ");
     $.getJSON("data/" + meta.capKey + "/dates.json", function(data){
-	var nowKey = makeDateKey(new Date());
-	var div = '<div>';
-	div += '<a id="' + nowKey + '" href="#">Today</a>';
-	div += '</div>';
-	$('#' + meta.datesId).append(div);
-	$("a#" + nowKey).click(function(event){
-	    event.preventDefault();
-	    stored.dateKey = this.id;
-	    updateStore();
-	    reload();
-	});
+	if (stored.dateKey == "" && data.dates.length > 0)
+	{
+	    stored.dateKey = data.dates[0];
+	}
 	for (var i = 0; i < data.dates.length; i++){
-	    if (data.dates[i] != nowKey) {
-		var div = '<div>';
-		div += '<a id="' + data.dates[i]+ '" href="#">' + data.dates[i] + '</a>';
-		div += '</div>';
-		$('#' + meta.datesId).append(div);
-		$("a#" + data.dates[i]).click(function(event){
-		    event.preventDefault();
-		    stored.dateKey = this.id;
-		    updateStore();
-		    reload();
-		});
-	    }
+	    var div = '<div>';
+	    div += '<a id="' + data.dates[i]+ '" href="#">' + data.dates[i] + '</a>';
+	    div += '</div>';
+	    $('#' + meta.datesId).append(div);
+	    $("a#" + data.dates[i]).click(function(event){
+		event.preventDefault();
+		stored.dateKey = this.id;
+		updateStore();
+		reload();
+	    });
 	}
 	reload();
     })
@@ -88,6 +71,11 @@ function loadDates() {
 function loadNames() {
     console.log("Loading " + meta.capKey + " names ... ");
     $.getJSON("data/" + meta.capKey + "/names.json", function(data){
+	if (stored.nameKey == "" && data.names.length > 0)
+	{
+	    stored.nameKey = data.names[0].key;
+	    stored.nameStr = data.names[0].name;
+	}
 	for (var i = 0; i < data.names.length; i++){
 	    var key = data.names[i].key;
 	    var div = '';
@@ -159,7 +147,7 @@ function reload() {
 	var buy = [];
 	var sell = [];
 	for (var i = 0; i < data.quotes.length; i++) {
-	    var date = makeDate(data.quotes[i].fetch_date, data.quotes[i].fetch_time);
+	    var date = makeDate(data.date, data.quotes[i].atime);
 	    console.log(date.getTime());
 	    volume.push([date.getTime(), (data.quotes[i].volume * 1)]);
 	    buy.push([date.getTime(), (data.quotes[i].buy * 1)]);
@@ -185,15 +173,6 @@ function plot(quotes) {
     var data = [];
     data.push({ label: "Sell", color: "green", data: quotes['sell'] });
     data.push({ label: "Buy", color: "blue", data: quotes['buy'] });
-/*
-    for (var i = 0; i < predicts.length; i++) {
-	if (i == 0) {
-	    data.push({ label: "Predict", color: "red", data: predicts[i] });
-	} else {
-	    data.push({ color: "red", data: predicts[i] });
-	}
-    }
-*/
     $.plot($("#" + meta.canvasId), data, {
 	series: {
 	    lines: { show: true },
